f867b29b22647ada958abffc43c32818ec729bd1
longtime1116 <longtime1116@gmail.com>
2023-06-07 10:22:14 +0900

    Userに関して、サンプルデータをinsertしてshowできるところまで

--- Gemfile
+++ Gemfile
@@ -59,3 +59,6 @@ end
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
 gem 'bootstrap-sass', '3.3.7'
+
+gem 'activerecord-import'
+
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,6 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+users = (1..3).map do |i|
+  User.new(name: "user_#{i}", email: "user_#{i}@example.com", password: 'hoge', password_confirmation: 'hoge')
+end
+User.import(users)
