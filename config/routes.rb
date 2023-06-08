f41d77dbf4d97bb73af5fd7c34e7776b9957c191
longtime1116 <longtime1116@gmail.com>
2023-06-08 11:44:50 +0900

    ログイン/ログアウト機能およびそれをブラウザから実行できるようにするviewを作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
-  get '/signup', to: 'users#new'
   resources :users, only: %i[index show create]
+  get '/signup', to: 'users#new'
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  get '/logout', to: 'sessions#destroy'
 end
