250a4addaf206eb9e097543c79acff9c49272016
longtime1116 <longtime1116@gmail.com>
2023-06-09 10:38:33 +0900

    クリエイターを作成できるようにした

---
+++ test/controllers/creators_controller_test.rb
@@ -0,0 +1,8 @@
+require 'test_helper'
+
+class CreatorsControllerTest < ActionDispatch::IntegrationTest
+  # test 'should get new' do
+  #  get new_creator_url
+  #  assert_response :success
+  # end
+end
